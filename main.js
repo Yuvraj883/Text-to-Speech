dcc154d5d24008ca4bb8c3966c3ec2e60a60241a
Yuvraj883 <kingyuvi88@gmail.com>
2022-07-08 18:09:33 +0530

    Tried switching to a stable built

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import '/style.css'
 
 let textArea = document.querySelector("#text-area");
 const convertBtn = document.querySelector("#convert-btn")
@@ -6,12 +6,14 @@ convertBtn.addEventListener("click", convertTextToSpeech);
 function convertTextToSpeech(){
   convertBtn.textContent = "Converting..."
   let speech = new SpeechSynthesisUtterance(); 
-  speech.lang = "hi"; 
-  speech.rate = 1; 
-  speech.pitch =1; 
+  speech.lang = "enUS"; 
+  speech.rate = .9; 
+  speech.pitch =.5; 
   speech.text = textArea.value; 
   speech.volume = 1; 
+  speech.voice= 
   speechSynthesis.speak(speech);
+  console.log("Button Clicked");
 
   setTimeout(function() {
     convertBtn.innerText = "Convert Text To Speech";
